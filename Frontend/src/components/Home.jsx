7644895738b234bbede446c86a3340c5b3924f66
RuaaAlsugheir <alsugheirruaa@gmail.com>
2023-03-14 23:28:00 +0300

    Basic styling and planning for Home component

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,6 +1,18 @@
 import React, {useEffect} from 'react'
+import {Link} from 'react-router-dom'
 import axios from 'axios'
+import '../styles/Home.scss'
 
 export default function Home() {
-  return <div className='ba-1px w-100'>Home</div>
+  return (
+    <div className="ba-1px w-100">
+      <div className="df flex-row justify-between align-items-center">
+        <h1>My Files</h1>
+        <Link to="/uploadFile">
+          <button>Add File</button>
+        </Link>
+      </div>
+      <section className="all-files">Home</section>
+    </div>
+  )
 }
