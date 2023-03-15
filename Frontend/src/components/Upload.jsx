b69963115ccaf1e4855c33b943b487727075cb3a
RuaaAlsugheir <alsugheirruaa@gmail.com>
2023-03-15 14:13:40 +0300

    Creating basic structure for the upload component

--- Frontend/src/components/Upload.jsx
+++ Frontend/src/components/Upload.jsx
@@ -1,6 +1,42 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import axios from 'axios'
 
 export default function Upload() {
-  return <div className='ba-1px df flex-col w-100'>Upload</div>
+  const [file, setFile] = useState({})
+
+  const validatePDF = file => {
+    const validType = 'application/pdf'
+    if (validType != file.type) {
+      return false
+    }
+    return true
+  }
+
+  const handleChange = e => {
+    const file = e.target.files[0]
+    if (validatePDF(file)) {
+      // Enable Submit button
+      console.log('This is a pdf file')
+      setFile(file);
+    } else {
+      // Disable Submit button and show an error message for the user
+      console.log('Not pdf')
+    }
+  }
+
+  return (
+    <div className="ba-1px df flex-col w-100">
+      <Link to="/">Back</Link>
+      <div className="df flex-col justify-center align-items-center">
+        Upload PDF File
+        <form className="df flex-col justify-center align-items-center">
+          <input className="pointer" type="file" onChange={handleChange} />
+          <button className="pointer" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
