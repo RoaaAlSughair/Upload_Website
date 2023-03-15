bcf5e8aee89851e59e6051a84ba7a87c5e38df18
RuaaAlsugheir <alsugheirruaa@gmail.com>
2023-03-15 18:21:10 +0300

    Adding the drag-and-drop feature

--- Frontend/src/components/Upload.jsx
+++ Frontend/src/components/Upload.jsx
@@ -13,17 +13,32 @@ export default function Upload() {
     return true
   }
 
-  const handleChange = e => {
-    const file = e.target.files[0]
+  const handleDrop = e => {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
     if (validatePDF(file)) {
       // Enable Submit button
       console.log('This is a pdf file')
-      setFile(file);
+      setFile(file)
     } else {
       // Disable Submit button and show an error message for the user
       console.log('Not pdf')
     }
   }
+
+  const handleChange = e => {
+    const file = e.target.files[0]
+    if (validatePDF(file)) {
+      // Enable Submit button
+      console.log('This is a pdf file')
+      setFile(file)
+    } else {
+      // Disable Submit button and show an error message for the user
+      console.log('Not pdf')
+    }
+  }
+
+  const handleSubmit = () => {}
 
   return (
     <div className="ba-1px df flex-col w-100">
@@ -31,8 +46,8 @@ export default function Upload() {
       <div className="df flex-col justify-center align-items-center">
         Upload PDF File
         <form className="df flex-col justify-center align-items-center">
-          <input className="pointer" type="file" onChange={handleChange} />
-          <button className="pointer" type="submit">
+          <input className="pointer" type="file" onChange={handleChange} onDrop={handleDrop}/>
+          <button className="pointer" type="submit" onSubmit={handleSubmit}>
             Submit
           </button>
         </form>
